4a810e80ab73474c80af77a1a050a21606906c84
Nate Blichfeldt <42626412+nrblichf@users.noreply.github.com>
2019-02-21 11:06:02 -0500

    Collectable Scripts
    Allows for the player to collect water, dirt, and bombs and updates the score when collecting these objects.

---
+++ TheNebulaProject/Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreScript : MonoBehaviour {
+    private int water_score = 0;
+    private int dirt_score = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("Water score: " + water_score +
+                  "\nDirt score: " + dirt_score); 
+	}
+
+    public void updateWaterScore(int update)
+    {
+        water_score += update;
+
+        if (water_score < 0)
+        {
+            water_score = 0;
+        }
+    }
+
+    public void updateDirtScore(int update)
+    {
+        dirt_score += update;
+
+        if (dirt_score < 0)
+        {
+            dirt_score = 0;
+        }
+    }
+}
